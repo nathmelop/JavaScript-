d25c5f8fed6b2f6cffce07a324e8118461254e83
nathmelop <nathaliapmelo@hotmail.com>
2021-01-16 17:26:06 -0300

    'enviando até aula 10'

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaScript-
 Todos meus testes comentados em cursos 
+
+-CSS e JS estão juntos no HTML 
+CSS <style>
+JS <script>
---
+++ let.js
@@ -0,0 +1,10 @@
+//usar o const quando o valor não tem q ser alterado 
+//let  quando for alterar valores 
+//const let valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+
+let nome = 'Nathalia'; //string literal 
+let idade = 24; //number literal 
+let estaAprovado = true; //boolean 
+let sobrenome = undefined; //undefined
+let corSelecionado = null // redefinir um valor
