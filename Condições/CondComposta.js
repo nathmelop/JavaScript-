c52b0c9f4bc8fba46becf08fbe59058d126e5675
nathmelop <nathaliapmelo@hotmail.com>
2021-01-19 13:40:46 -0300

    'if/else + js/html'

---
+++ Condições/CondComposta.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') { // se o pais for igual(==) a brasil ou pode usar o != (diferente de brasil)
+    console.log('você é brasileiro')
+} else {
+    console.log('você é estrangeiro')
+}
---
+++ Condições/CondSimples.js
@@ -0,0 +1,12 @@
+//Condições 
+//IF - se  ( (caso seja true )  // else - se não  (caso a cond seja false)
+
+//console.log('o console funcionou corretamente') // f8 para rodar  
+
+
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //condição simples 
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
