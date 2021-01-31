80d521c12305e85c3939665980289dc6b4aad221
nathmelop <nathaliapmelo@hotmail.com>
2021-01-31 17:19:05 -0300

    n roda de acordo com o horario

--- Exercicios/DataHora/script.js
+++ Exercicios/DataHora/script.js
@@ -3,13 +3,13 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date() /* data atual */
         // var hora = data.getHours() /* hora atual */
-    var hora = 10
+    var hora = 20
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora > 0 && hora < 12) {
         // bom dia!
-        document.innerHTML = '<img src "manha.png"'
+        img.src = './manha.png'
         document.body.style.background = '#e6d2aa'
     } else if (hora >= 12 && hora <= 18) {
         // boa tarde!
